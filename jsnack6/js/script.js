c00d7b1bf425be2359a9ab2939cdf93ccdfb1e24
DanieleBaggiano <151195732+DanieleBaggiano@users.noreply.github.com>
2024-04-02 18:11:02 +0200

    snack 6

---
+++ jsnack6/js/script.js
@@ -0,0 +1,9 @@
+// JSnack 6
+// Stampa il cubo dei primi N numeri,
+// dove N è un numero indicato dall'utente.
+
+const userNum = parseInt(prompt("dimmi un numero"));
+
+for (let i = 1; i <= userNum; i++);
+const cubo = userNum ** 3;
+console.log(`Il cubo di ${userNum} è ${cubo}`);
